7209ea9ed3ac6889a3084fcb9f890deddd937379
Scott Endicott <sendicott1391@gmail.com>
2016-09-09 13:59:18 -0400

    Setting up environment with gulp and sass and foundation

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
+'use strict'
+
 let gulp = require('gulp');
 let browserify = require('gulp-browserify');
+let sass = require('gulp-sass');
 
 gulp.task('default', ['html', 'css', 'js']);
 
 gulp.task('html', function () {
-    // start with a file called index.html
     return gulp.src('index.html')
         .pipe(gulp.dest('public/'));
 });
 
 gulp.task('css', function() {
-    return gulp.src('style.css')
+    return gulp.src('style.scss')
+        .pipe(sass())
         .pipe(gulp.dest('public/'));
 });
 
@@ -24,5 +27,5 @@ gulp.task('watch', function () {
     // when index.html changes, run html task
     gulp.watch('index.html', ['html']);
     gulp.watch('app.js', ['js']);
-    gulp.watch('style.css', ['css']);
+    gulp.watch('style.scss', ['css']);
 });
